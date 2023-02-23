ce50ae5ed38031b61b86ef57b06e3763b365c6c1
torokbi <bence.torok50@gmail.com>
2023-02-23 20:41:01 +0100

    create sample datas for testing

---
+++ data.py
@@ -0,0 +1,58 @@
+events = [
+    {
+        'name': 'Isten jelenléte a sejtbiológiában ',
+        'tutor': 'Horváth Hanna',
+        'colum': 1,
+        'description': 'Isten jelenléte a sejtbiológiában egy a területen meghatározó személy tekintetében.',
+        'location': '9/a',
+        'seat': 20
+    },
+    {
+        'name': 'Generációs szakadékok',
+        'tutor': 'Tihamér Adnrás',
+        'colum': 1,
+        'description': 'Mi az ami elvákasztja a mai fiatalokat a szüleiktől?',
+        'location': 'tornaterem',
+        'seat': 0
+    },
+    {
+        'name': 'Isten jelenléte a sejtbiológiában ',
+        'tutor': 'Horváth Hanna',
+        'colum': 2,
+        'description': 'Isten jelenléte a sejtbiológiában egy a területen meghatározó személy tekintetében.',
+        'location': '9/a',
+        'seat': 20
+    },
+    {
+        'name': 'Generációs szakadékok',
+        'tutor': 'Tihamér Adnrás',
+        'colum': 2,
+        'description': 'Mi az ami elvákasztja a mai fiatalokat a szüleiktől?',
+        'location': 'tornaterem',
+        'seat': 0
+    },
+    {
+        'name': 'Gyónás - Ciril Atya ',
+        'tutor': None,
+        'colum': 3,
+        'description': None,
+        'location': 'német2',
+        'seat': 0
+    },
+    {
+        'name': 'Gyónás - Gábor Atya ',
+        'tutor': None,
+        'colum': 3,
+        'description': None,
+        'location': 'német1',
+        'seat': 0
+    },
+    {
+        'name': 'Gyónás - Róber Atya ',
+        'tutor': None,
+        'colum': 3,
+        'description': None,
+        'location': 'angol1',
+        'seat': 0
+    }
+]
