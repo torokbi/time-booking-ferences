393c1305d393b815f1da173970a4b1d7f9a536d2
torokbi <bence.torok50@gmail.com>
2023-02-23 20:40:33 +0100

    Make login page

--- booking.py
+++ booking.py
@@ -1,8 +1,21 @@
 from flask import Flask, redirect, url_for, render_template
+from data import events
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("student.html",events=events)
 
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/forgot-pw")
+def forgot_pw():
+    return render_template("forgot.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
