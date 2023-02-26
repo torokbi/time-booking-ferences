b6833919b6e45c341955ff98b277ebd929865dcf
torokbi <bence.torok50@gmail.com>
2023-02-26 19:19:00 +0100

    Create the main page of event funcions.

--- booking.py
+++ booking.py
@@ -8,6 +8,21 @@ def home():
     return render_template("student.html",events=events)
 
 
+@app.route("/events")
+def event_manage ():
+    datas = events.copy()
+    for first in range(0,len(datas)):
+        for second in range(first+1,len(datas)):
+            if datas[first]["colum"] > datas[second]["colum"]:
+                    datas[first],datas[second] = datas[second],datas[first]
+    for first in range(0,len(datas)):
+        for second in range(first+1,len(datas)):
+            if datas[first]["name"] > datas[second]["name"] and datas[first]["colum"] == datas[second]["colum"]:
+                    datas[first],datas[second] = datas[second],datas[first]
+
+    return render_template("events.html",events=datas)
+
+
 @app.route("/login")
 def login():
     return render_template("login.html")
